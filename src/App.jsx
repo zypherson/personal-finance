1e3746e9aac8c3c2e154afdfc00b74aebcfe96bd
zypherson <taylorzypherson@gmail.com>
2026-01-15 02:57:26 -0500

    rendered transactionlist in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { getTransactions, saveTransactions } from "./utils/storage";
 import TransactionForm from "./components/TransactionForm";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   const [transactions, setTransactions] = useState(() =>
@@ -18,6 +19,7 @@ function App() {
     <div>
       <h1>Personal Finance Snapshot</h1>
       <TransactionForm onAdd={addTransaction} />
+      <TransactionList transactions={transactions} />
       <p>Transactions: {transactions.length}</p>
     </div>
   );
