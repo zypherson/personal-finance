f9382c0ba8dc2bc6842ffd61a3a15bce198ef83a
zypherson <taylorzypherson@gmail.com>
2026-01-21 00:34:42 -0500

    added delete function i app jsx and passed it to the transaction list

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,19 @@ function App() {
     setTransactions(updated);
     saveTransactions(updated);
   }
+  function deleteTransaction(id) {
+    const updated = transactions.filter((t) => t.id !== id);
+    setTransactions(updated);
+    saveTransactions(updated);
+  }
 
   return (
     <div>
       <h1>Personal Finance Snapshot</h1>
       <TransactionForm onAdd={addTransaction} />
-      <TransactionList transactions={transactions} />
+      <TransactionList transactions={transactions} onDelete={deleteTransaction} />
       <Summary transactions={transactions} />
-
+ 
       <p>Transactions: {transactions.length}</p>
     </div>
   );
